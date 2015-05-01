0729d1957dc649f3a287536e4365d9aa4a46ff5c
Ryan Haase <ryandhaase@gmail.com>
2015-04-30 20:29:49 -0400

    Updated user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
+      post :create, user: { token: "testing" }
       expect(response).to have_http_status(:success)
     end
   end
